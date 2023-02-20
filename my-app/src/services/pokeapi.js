c808037e11767e09e130680fe8057994a6465b95
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-02-20 19:01:31 -0300

    melhorando css

--- my-app/src/services/pokeapi.js
+++ my-app/src/services/pokeapi.js
@@ -3,7 +3,7 @@ const getpokemons = async ()=>{
 
 // url da api com todos os pokemons  
 // const dado  = await fetch(' https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0.')
-const dado  = await fetch(' https://pokeapi.co/api/v2/pokemon/')
+const dado  = await fetch('https://pokeapi.co/api/v2/pokemon/')
 const response = await dado.json()
 console.log(response);
 return response
