cf4d2732d73a07e3ecb1315c41d0537e6609de30
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-02-23 20:39:55 -0300

    add try e catch

--- my-app/src/services/pokeapi.js
+++ my-app/src/services/pokeapi.js
@@ -3,10 +3,15 @@ const getpokemons = async ()=>{
 
 // url da api com todos os pokemons  
 // const dado  = await fetch(' https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0.')
-const dado  = await fetch('https://pokeapi.co/api/v2/pokemon/')
-const response = await dado.json()
-console.log(response);
-return response
+try{
+
+    const dado  = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    const response = await dado.json()
+    console.log(response);
+    return response
+}catch(error){
+console.error(error);
+}
 }
 
 export default getpokemons;
