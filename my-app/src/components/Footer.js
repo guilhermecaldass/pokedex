64903064e087b64ee8ad10b45c8f6ed689a18558
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-02-27 21:00:29 -0300

    add footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,13 +1,14 @@
-
-
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
-      
+      <p>
+        Este é um projeto que manipula a PokéApi uma api de pokemons. Ela é uma API
+        publica, e esse projeto não tem fins lucrativos.
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
