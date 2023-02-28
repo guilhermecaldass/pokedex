d2a289a75267d812e146421359c6c40049396d3a
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-02-28 19:04:54 -0300

    add stilo no footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import style from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={style.footer}>
       <p>
         Este é um projeto que manipula a PokéApi uma api de pokemons. Ela é uma API
         publica, e esse projeto não tem fins lucrativos.
