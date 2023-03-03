41d6bd4dea9073fcf9cac409453b32fe2d2c3ae4
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-03-03 17:17:21 -0300

    ajustando footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,8 +26,8 @@ const App=()=> {
           </>
         )}
 
-        <Footer />
       </div>
+        <Footer />
     </div>
   );
 }
--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import style from './Footer.module.css'
+import img from '../imgs/imgFooter.png'
 
 const Footer = () => {
   return (
     <footer className={style.footer}>
-      <p>
+      <p className={style.p}>
         Este é um projeto que manipula a PokéApi uma api de pokemons. Ela é uma API
         publica, e esse projeto não tem fins lucrativos.
       </p>
+      <img className={style.imgFooter} src={img} alt='pokeball'></img>
+      <p className={style.p}>Desenvolvedor: Guilherme Caldas</p>
     </footer>
   );
 };
